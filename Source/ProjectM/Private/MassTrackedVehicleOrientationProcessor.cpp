d6860684f8a88435bd952c61f3ac344febfa71b2
LeroyTechnologies <108254625+LeroyTechnologies@users.noreply.github.com>
2022-09-11 17:47:39 -0700

    Tanks: Add UMassTrackedVehicleOrientationProcessor to turn tracked vehicles at reasonable speed toward target

---
+++ Source/ProjectM/Private/MassTrackedVehicleOrientationProcessor.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MassTrackedVehicleOrientationProcessor.h"
+
+void UMassTrackedVehicleOrientationTrait::BuildTemplate(FMassEntityTemplateBuildContext& BuildContext, UWorld& World) const
+{
+	UMassEntitySubsystem* EntitySubsystem = UWorld::GetSubsystem<UMassEntitySubsystem>(&World);
+	check(EntitySubsystem);
+
+	BuildContext.AddFragment<FMassMoveTargetFragment>();
+	BuildContext.AddFragment<FTransformFragment>();
+
+	const FConstSharedStruct OrientationFragment = EntitySubsystem->GetOrCreateConstSharedFragment(UE::StructUtils::GetStructCrc32(FConstStructView::Make(Orientation)), Orientation);
+	BuildContext.AddConstSharedFragment(OrientationFragment);
+}
+
+UMassTrackedVehicleOrientationProcessor::UMassTrackedVehicleOrientationProcessor()
+{
+	ExecutionFlags = (int32)EProcessorExecutionFlags::All;
+	ExecutionOrder.ExecuteInGroup = UE::Mass::ProcessorGroupNames::Movement;
+}
+
+void UMassTrackedVehicleOrientationProcessor::ConfigureQueries()
+{
+	EntityQuery.AddRequirement<FMassMoveTargetFragment>(EMassFragmentAccess::ReadOnly);
+	EntityQuery.AddRequirement<FTransformFragment>(EMassFragmentAccess::ReadWrite);
+	EntityQuery.AddConstSharedRequirement<FMassTrackedVehicleOrientationParameters>(EMassFragmentPresence::All);
+}
+
+void UMassTrackedVehicleOrientationProcessor::Execute(UMassEntitySubsystem& EntitySubsystem,
+	FMassExecutionContext& Context)
+{
+	QUICK_SCOPE_CYCLE_COUNTER(UMassTrackedVehicleOrientationProcessor_Execute);
+
+	// Clamp max delta time to avoid large values during initialization.
+	const float DeltaTime = FMath::Min(0.1f, Context.GetDeltaTimeSeconds());
+
+	EntityQuery.ForEachEntityChunk(EntitySubsystem, Context, [DeltaTime](FMassExecutionContext& Context)
+	{
+		const int32 NumEntities = Context.GetNumEntities();
+
+		const FMassTrackedVehicleOrientationParameters& OrientationParams = Context.GetConstSharedFragment<FMassTrackedVehicleOrientationParameters>();
+
+		const TConstArrayView<FMassMoveTargetFragment> MoveTargetList = Context.GetFragmentView<FMassMoveTargetFragment>();
+		const TArrayView<FTransformFragment> LocationList = Context.GetMutableFragmentView<FTransformFragment>();
+
+		for (int32 EntityIndex = 0; EntityIndex < NumEntities; ++EntityIndex)
+		{
+			const FMassMoveTargetFragment& MoveTarget = MoveTargetList[EntityIndex];
+
+			FTransform& CurrentTransform = LocationList[EntityIndex].GetMutableTransform();
+			const FVector CurrentForward = CurrentTransform.GetRotation().GetForwardVector();
+			
+			const float CurrentHeading = UE::MassNavigation::GetYawFromDirection(CurrentForward);
+			const float DesiredHeading = UE::MassNavigation::GetYawFromDirection(MoveTarget.Forward);
+
+			if (FMath::IsNearlyEqual(CurrentHeading, DesiredHeading))
+			{
+				return;
+			}
+
+			float NewHeading;
+			float DeltaHeading = FMath::DegreesToRadians(OrientationParams.TurningSpeed) * DeltaTime;
+			if (FMath::Abs(CurrentHeading - DesiredHeading) <= DeltaHeading)
+			{
+				NewHeading = DesiredHeading;
+			}
+			else
+			{
+				DeltaHeading = DesiredHeading > CurrentHeading ? DeltaHeading : -DeltaHeading;
+				NewHeading = CurrentHeading + DeltaHeading;
+			}
+
+			FQuat Rotation(FVector::UpVector, NewHeading);
+			CurrentTransform.SetRotation(Rotation);
+		}
+	});
+}
---
+++ Source/ProjectM/Public/MassTrackedVehicleOrientationProcessor.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassProcessor.h"
+#include "MassTrackedVehicleOrientationProcessor.generated.h"
+
+USTRUCT()
+struct PROJECTM_API FMassTrackedVehicleOrientationParameters : public FMassSharedFragment
+{
+	GENERATED_BODY()
+
+	/** Measured in degrees per second. */
+	UPROPERTY(EditAnywhere, Category = "Orientation")
+	float TurningSpeed;
+};
+
+UCLASS(meta = (DisplayName = "Tracked Vehicle Orientation"))
+class PROJECTM_API UMassTrackedVehicleOrientationTrait : public UMassEntityTraitBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BuildTemplate(FMassEntityTemplateBuildContext& BuildContext, UWorld& World) const override;
+
+	UPROPERTY(EditAnywhere, Category = "")
+	FMassTrackedVehicleOrientationParameters Orientation;
+};
+
+UCLASS()
+class PROJECTM_API UMassTrackedVehicleOrientationProcessor : public UMassProcessor
+{
+	GENERATED_BODY()
+
+public:
+	UMassTrackedVehicleOrientationProcessor();
+
+protected:
+	virtual void ConfigureQueries() override;
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+
+private:
+	FMassEntityQuery EntityQuery;
+};
